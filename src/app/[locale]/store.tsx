8090619e47bb8e1c077dfab14c5cd785fe1c8274
Oleh Proidakov <first.god.hermit@gmail.com>
2023-07-10 20:12:04 +0300

    Synchronized NEXT_LOCALE and settings.locale

--- src/app/[locale]/store.tsx
+++ src/app/[locale]/store.tsx
@@ -4,12 +4,18 @@ import articlesSlice, { initialArticlesState } from '@/_helpers/articlesSlice';
 import settingsSlice, { initialSettings } from '@/_helpers/settingsSlice';
 import sideNavSlice, { initialSideNavState } from '@/components/SideNav/sideNavSlice';
 import { configureStore } from '@reduxjs/toolkit';
+import { getCookie, setCookie } from 'cookies-next';
 
 export const preloadedState = {
 	sideNav: initialSideNavState,
 	settings: initialSettings,
 	articles: initialArticlesState
 }
+
+const preloadedSettings = JSON.parse( // Load settings from localStorage
+	typeof window !== 'undefined'
+	&& localStorage.getItem('settings') || '{}'
+);
 
 const store = configureStore({
 	reducer: {
@@ -19,8 +25,9 @@ const store = configureStore({
 	},
 	preloadedState: {
 		settings: {
-			...initialSettings,
-			...JSON.parse(typeof window !== 'undefined' && localStorage.getItem('settings') || '{}')
+			...initialSettings, // Load default settings
+			...preloadedSettings, // Load settings from localStorage
+			locale: getCookie('NEXT_LOCALE') || preloadedSettings.locale || initialSettings.locale, // Load locale from cookie
 		}
 	}
 });
@@ -31,6 +38,26 @@ const store = configureStore({
 store.subscribe(() => {
 	const state = store.getState();
 	localStorage.setItem('settings', JSON.stringify(state.settings));
+});
+
+/**
+ * Synchronize NEXT_LOCALE cookie and settings.locale
+ */
+store.subscribe(() => {
+	const state = store.getState(); // Get current state
+	const localeInCookie = getCookie('NEXT_LOCALE'); // Get locale from cookie
+
+	if (localeInCookie !== state.settings.locale) {
+		// If locale in cookie is different from locale in settings, update cookie
+		setCookie( // Set cookie with locale from settings
+			'NEXT_LOCALE',
+			state.settings.locale,
+			{
+				path: '/',
+				sameSite: 'strict',
+			}
+		);
+	}
 });
 
 export default store;
